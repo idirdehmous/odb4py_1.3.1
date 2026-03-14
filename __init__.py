fcc87d723b44604b9c0e4ca7f8d408b1d20dcf11
idir Dehmous <idirdehmous@gmail.com>
2026-03-14 19:23:00 +0100

    Change the version  to 1.3.1

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 #
 # Licensed under the Apache License, Version 2.0
 
-__version__ = "1.1.2"
+__version__ = "1.3.1"
 
 # Ensure that the env is initialized whichever module is called !
 from .utils.setting_env import OdbEnv
